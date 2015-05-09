557ce64a892afccc51810561d4a834601cf05ab0
Francisco Leal <francisco.leal@ist.utl.pt>
2015-05-09 13:17:43 +0100

    starting GHC

--- POO_Project/src/project/GHC.java
+++ POO_Project/src/project/GHC.java
@@ -4,7 +4,28 @@ public class GHC implements Train {
 
 	public void train()
 	{
+		/*	Input: Initial Structure ninit, dataset D, Scoring function, stopping criteria  
+		 * 	Output: Nres
+		 * 
+		 * 	Nres = Ninit , N' = Nres
+		 * 	while stopping criteria is not satisfied
+		 * 		N'' = arg max scoring(N)
+		 * 		if scoring(N') > scoring(N'') then N'' = random(N)
+		 * 		if scoring(N'') > scoring(Nres) then Nres = N''
+		 * 		N' = N''
+		 * 	end while
+		 * 	return Nres
+		 */
 		
+		TransitionNetwork nRes, nMax, n = null;
+		while(true){
+			nMax = calcMaxNeighbourhood(n);
+		}
+	}
+	
+	public TransitionNetwork calcMaxNeighbourhood(TransitionNetwork nInit){
+		
+		return null;
 	}
 	
 	Score score;
