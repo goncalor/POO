bf931ae5beb58a55a988341eab81d47a504a95eb
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2015-05-12 22:35:18 +0000

    int[] stored in each node in TN. Added TN.toString
    Must still check if index is valid in the constructor.

--- Project/src/project/TransitionNetwork.java
+++ Project/src/project/TransitionNetwork.java
@@ -1,6 +1,7 @@
 package project;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.IdentityHashMap;
 import java.util.Map;
 
@@ -9,23 +10,29 @@ public class TransitionNetwork extends Network {
 	/** the nodes in this transition network. first half of the array refers to time t; second half to time t+1 */
 	private Node[] nodes;
 	CheckStructure checkDAG;
-	private Slice past;
-	private Slice present;
+//	private Slice sliceT;
+//	private Slice sliceT1;
 	int[] varDomain;
 	
 	/** */
 	public TransitionNetwork(Data data, int index) {
 		
 		//TODO verify if a slice for index+1 exists
-		past = data.get(index);
-		present = data.get(index+1);
+		Slice sliceT = data.get(index);
+		Slice sliceT1 = data.get(index+1);
 		nodes = new Node[2*Slice.numVar];
 		checkDAG = new Tarjan();	// Tarjan is the default algorithm for checkDAG()
 		
 		varDomain = data.getVarDomain();
 		
-		for(int i=0; i<nodes.length; i++)	// create nodes for this TN
-			nodes[i] = new Node();	
+		for(int i=0; i<nodes.length; i++) {	// create nodes for this TN
+			nodes[i] = new Node();
+		}
+		
+		for(int i=0; i<nodes.length/2; i++) {	// create nodes for this TN
+			nodes[i].content = sliceT.get(i);
+			nodes[i + nodes.length/2].content = sliceT1.get(i);
+		}		
 	}
 	
 	/** @return the i th node in the network */
@@ -135,8 +142,15 @@ public class TransitionNetwork extends Network {
 		T.execute(this, S);
 	}
 	
-	
-	
+	@Override
+	public String toString() {
+		String s = "";
+		for(Node n: nodes) {
+			s += Arrays.toString((int[]) n.content) + ", ";
+		}
+		return "TransitionNetwork [nodes=" + s + "]";
+	}
+
 	/** main() for testing purposes 
 	 * @throws NodeOutOfBoundsException */
 	public static void main(String[] args) throws NodeOutOfBoundsException {
@@ -144,7 +158,7 @@ public class TransitionNetwork extends Network {
 		Data data = null;
 		Parser parse = new Parser();
 		try {
-			data = parse.fromFile(new String("datasets/test01.csv"));
+			data = parse.fromFile(new String("datasets/test02.csv"));
 		} catch (IOException e) {
 			System.out.println("No such file");
 			System.exit(-1);
@@ -152,14 +166,23 @@ public class TransitionNetwork extends Network {
 		
 		System.out.println("#Different sets of elements:\n" + data);
 		
+		/* transition from t=0 to t=1 */
 		TransitionNetwork tn = new TransitionNetwork(data, 0);
+		
+		System.out.println(tn);
 		
 		boolean retval;
 		retval = tn.addEdge(tn.nodes[0], tn.nodes[1]);
 		System.out.println(retval);
 		
-		retval = tn.addEdge(tn.nodes[1], tn.nodes[0]);
+		retval = tn.addEdge(tn.nodes[1], tn.nodes[2]);
 		System.out.println(retval);
 		
+		retval = tn.addEdge(tn.nodes[2], tn.nodes[0]);
+		System.out.println(retval);
+				
+		/* transition from t=1 to t=2 */
+		
+		TransitionNetwork tn1 = new TransitionNetwork(data, 1);
 	}
 }
