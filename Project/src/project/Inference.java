99bd3c1641d40a745b1425382b6688d9a6900597
Francisco Leal <francisco.leal@ist.utl.pt>
2015-05-17 23:00:10 +0100

    inference started

--- Project/src/project/Inference.java
+++ Project/src/project/Inference.java
@@ -1,8 +1,61 @@
 package project;
 
+import java.util.Iterator;
+
 public class Inference {
 	
-	public static void calcInference(){
+	public static void calcInference(TransitionNetwork tn){
+		float allThetas[][][] = Theta.calcAllThetas(tn);
+		int nrNodes = tn.nrNodes();
+		
+		int i = 1; //fixar i
+		
+		int combos=1;
+		for(int sizeOfMat = 0; sizeOfMat < nrNodes/2; sizeOfMat++){
+			if( sizeOfMat != i){
+				combos *= (tn.varDomain[sizeOfMat]+1);
+			}
+		}
+		System.out.println("Combos: " + combos);
+		
+		int k = 0; // fixar k
+		int ji[] = new int[nrNodes/2];
+		for(int iter=0; iter<nrNodes/2;iter++){
+			if(iter == i){
+				ji[iter] = k;
+			}else{
+				ji[iter] = (tn.varDomain[iter]);
+			}
+		}
+		
+		System.out.println("all vals");
+		for(int a : ji){
+			System.out.print(" " + a);
+		}
+		
+//		jMax = Nijk.convertJitoJ(ji, maxValues);
+		
+//		
+//		for(int p=nrNodes/2; p < nrNodes;p++ ){
+//			Node tempNode = tn.getNode(p);
+//			for(Iterator<Node> iter=tempNode.iterator(); iter.hasNext(); ){
+//				Node parent = iter.next();
+//				if(parent.selfIndex < nrNodes/2){
+//					System.out.println("get from test data " + p);
+//				}else{
+//					System.out.println("get all vals " + p);
+//				}
+//			}
+//		}
+//		
+//		for(int i=0; i < nrNodes/2;i++){		// i is the node
+//			for(int k=0; k<=tn.varDomain[i];k++){	// k is the  kth node val
+//				
+//				
+//				
+//				
+//			}
+//		}
 		
 	}
 	
--- Project/src/project/Theta.java
+++ Project/src/project/Theta.java
@@ -44,6 +44,48 @@ public class Theta {
 		
 		return returnVal;
 	}
+	
+	public static float[][][] calcAllThetas(TransitionNetwork tn){
+		float returnVal[][][] = new float[tn.nrNodes()/2][][];
+		for(int i=tn.nrNodes()/2; i < tn.nrNodes();i++){
+			returnVal[i-tn.nrNodes()/2] = Theta.calcThetasForNodeI(tn, i);
+		}
+		return returnVal;
+	}
+	
+	private static float[][] calcThetasForNodeI(TransitionNetwork tn, int i){
+		int j, maxJ;
+		Node currentNode = tn.getNode(i);
+		if(currentNode.nrEdges() == 0){
+			maxJ = 0;
+			float [][]AllThetas = new float[maxJ+1][tn.varDomain[currentNode.getIndex()%tn.varDomain.length]+1];
+			for(int k=0; k<=tn.varDomain[currentNode.getIndex()%tn.varDomain.length];k++){
+//				System.out.println("i: "+i+" j: "+maxJ + " k: "+k+ " " +Theta.calcThetaIJK(i-tn.nrNodes()/2, maxJ, k, tn));
+				AllThetas[maxJ][k] = Theta.calcThetaIJK(i-tn.nrNodes()/2, maxJ, k, tn);
+			}
+			return AllThetas;
+		}else{
+
+			int parentsMax[] = new int[currentNode.nrEdges()];
+			int iterator=0;
+			for(Iterator<Node> iter=currentNode.iterator(); iter.hasNext(); ){
+				Node parent = iter.next();
+				parentsMax[iterator] = tn.varDomain[parent.getIndex()%tn.varDomain.length];
+				iterator++;
+			}
+			maxJ = Nijk.convertJitoJ(parentsMax, parentsMax);
+
+			float [][]AllThetas = new float[maxJ][tn.varDomain[currentNode.getIndex()%tn.varDomain.length]+1];
+			for(j=0; j < maxJ; j++){
+				for(int k=0; k<= tn.varDomain[currentNode.getIndex()%tn.varDomain.length];k++){
+//					System.out.println("i: "+i+" j: "+j + " k: "+k+ " " + Theta.calcThetaIJK(i-tn.nrNodes()/2, j, k, tn));
+					AllThetas[j][k] = Theta.calcThetaIJK(i-tn.nrNodes()/2, j, k, tn);
+				}
+			}
+			return AllThetas;
+		}
+		
+	}
 
 	public static void main(String[] args) throws IOException, NodeOutOfBoundsException{
 		String s = new String("datasets/train-data-fortest.csv");
@@ -60,32 +102,57 @@ public class Theta {
 		
 		System.out.println(tn);
 		
-		int j, maxJ;
+		float allThetas[][][] = Theta.calcAllThetas(tn);
 		
-		for(int i=tn.nrNodes()/2; i < tn.nrNodes();i++){
-			Node currentNode = tn.getNode(i);
-			if(currentNode.nrEdges() == 0){
-				maxJ = 0;
-				for(int k=0; k<=tn.varDomain[currentNode.getIndex()%tn.varDomain.length];k++){
-					System.out.println("i: "+i+" j: "+maxJ + " k: "+k+ " " +Theta.calcThetaIJK(i-tn.nrNodes()/2, maxJ, k, tn));
-				}
-			}else{
-
-				int parentsMax[] = new int[currentNode.nrEdges()];
-				int iterator=0;
-				for(Iterator<Node> iter=currentNode.iterator(); iter.hasNext(); ){
-					Node parent = iter.next();
-					parentsMax[iterator] = tn.varDomain[parent.getIndex()%tn.varDomain.length];
-					iterator++;
-				}
-				maxJ = Nijk.convertJitoJ(parentsMax, parentsMax);
-				for(j=0; j < maxJ; j++){
-					for(int k=0; k<= tn.varDomain[currentNode.getIndex()%tn.varDomain.length];k++){
-						System.out.println("i: "+i+" j: "+j + " k: "+k+ " " + Theta.calcThetaIJK(i-tn.nrNodes()/2, j, k, tn));
-					}
-				}
-			}
-		}
+		Inference.calcInference(tn);
+		
+//		int i=0,j = 0,k=0;
+//		for(float[][] a:allThetas){
+//			for(float[] b : a){
+//				for(float c : b){
+//					System.out.println("i: "+i+" j: "+j + " k: "+k+ " " +c);
+//					k++;
+//				}
+//				k=0;
+//				j++;
+//			}
+//			j=0;
+//			i++;
+//		}
+		
+//		System.out.println(allThetas[1][0][0]*allThetas[0][0][0]*allThetas[2][0][0]
+//				+allThetas[1][0][0]*allThetas[0][0][0]*allThetas[2][0][1]
+//				+allThetas[1][1][0]*allThetas[0][0][1]*allThetas[2][1][0]
+//				+allThetas[1][1][0]*allThetas[0][0][1]*allThetas[2][1][1]);
+		
+//		int j, maxJ;
+//		
+//		for(int i=tn.nrNodes()/2; i < tn.nrNodes();i++){
+//			Node currentNode = tn.getNode(i);
+//			if(currentNode.nrEdges() == 0){
+//				maxJ = 0;
+//				for(int k=0; k<=tn.varDomain[currentNode.getIndex()%tn.varDomain.length];k++){
+////					System.out.println("i: "+i+" j: "+maxJ + " k: "+k+ " " +Theta.calcThetaIJK(i-tn.nrNodes()/2, maxJ, k, tn));
+//					Theta.calcThetaIJK(i-tn.nrNodes()/2, maxJ, k, tn);
+//				}
+//			}else{
+//
+//				int parentsMax[] = new int[currentNode.nrEdges()];
+//				int iterator=0;
+//				for(Iterator<Node> iter=currentNode.iterator(); iter.hasNext(); ){
+//					Node parent = iter.next();
+//					parentsMax[iterator] = tn.varDomain[parent.getIndex()%tn.varDomain.length];
+//					iterator++;
+//				}
+//				maxJ = Nijk.convertJitoJ(parentsMax, parentsMax);
+//				for(j=0; j < maxJ; j++){
+//					for(int k=0; k<= tn.varDomain[currentNode.getIndex()%tn.varDomain.length];k++){
+////						System.out.println("i: "+i+" j: "+j + " k: "+k+ " " + Theta.calcThetaIJK(i-tn.nrNodes()/2, j, k, tn));
+//						Theta.calcThetaIJK(i-tn.nrNodes()/2, j, k, tn);
+//					}
+//				}
+//			}
+//		}
 		
 	}
 
