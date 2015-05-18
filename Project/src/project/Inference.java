8f1ccc2c5226c01909a23877f99dfb8fc8378cb3
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2015-05-18 15:14:22 +0000

    Thetas indexes are correct.

--- Project/src/project/Inference.java
+++ Project/src/project/Inference.java
@@ -31,7 +31,7 @@ public class Inference {
 //		for(int i=0; i<nrT1Configs; i++)
 //			System.out.println(Arrays.toString(d[i]));
 		
-		int i = 1, j, k = 0;
+		int i = 1, j, k = 0, jprime;
 		int testLine = 0;	// the line of the test data we're int
 		
 		int[] testDataLine = slice.getLine(testLine);
@@ -51,8 +51,8 @@ public class Inference {
 			{
 				continue;
 			}
-			// calculate j for Oijk
-		
+			
+			// calculate j for Oijk		
 			nrParents = t1[i].nrEdges();
 			parentValues = new int[nrParents];
 			parentMaxs= new int[nrParents];
@@ -89,7 +89,12 @@ public class Inference {
 			
 			Oijk = thetas[i][j][k];
 
-			System.out.println(i+" " +j+" "+ k+" "+Oijk);
+			System.out.println("Oijk  " +i+" " +j+" "+ k+" "+Oijk);
+			
+			
+			
+			
+			float OljPrimedl;
 			
 			// iterate over all nodes in t+1 except for the ith node
 			for(int l=0; l<t1.length; l++)
@@ -99,8 +104,56 @@ public class Inference {
 					continue;
 				}
 				
+				int nrT1ConfigsPrime = 1;	// the number of conbinations of time T+1
+				for(int tmp=0; tmp<varDomain.length; tmp++)
+					nrT1ConfigsPrime *= varDomain[tmp]+1;
 				
+				// d will have all T1 configs
+				int[][] dprime = new int[nrT1ConfigsPrime][nrNodes];
 				
+				// populate d
+				for(int tmp=0; tmp<nrT1ConfigsPrime; tmp++)
+					dprime[tmp] = Nijk.convertJtoJi(tmp, varDomain);
+
+				// calculate j' for Ol,j',d(l)
+				int nrParentsPrime = t1[l].nrEdges();
+				int[] parentValuesPrime = new int[nrParentsPrime];
+				int[] parentMaxsPrime = new int[nrParentsPrime];
+				
+				int auxIndex2 = 0;
+				for(Iterator<Node> iter=t1[l].iterator(); iter.hasNext(); )
+				{
+					Node parent = iter.next();
+					
+					// populate parentValues and parentMaxs
+					if(parent.selfIndex < nrNodes/2)
+					{
+//						System.out.println("parent in t");
+						// parent is from t. get the value from test data
+						parentValuesPrime[auxIndex2] = testDataLine[parent.selfIndex];
+						parentMaxsPrime[auxIndex2] = varDomain[parent.selfIndex];
+					}
+					else
+					{
+//						System.out.println("parent in t+1");
+						// parent is from t+1. get value from d
+						parentValuesPrime[auxIndex2] = dprime[t1Config][parent.selfIndex-nrNodes/2];
+						parentMaxsPrime[auxIndex2] = varDomain[parent.selfIndex-nrNodes/2];
+					}
+					auxIndex2++;
+				}
+
+				// now we got the value of j'
+				jprime = Nijk.convertJitoJ(parentValuesPrime, parentMaxsPrime);
+								
+//				System.out.println(Arrays.toString(parentValues));
+////				System.out.println(Arrays.toString(parentMaxs));
+//				System.out.println(j);
+				
+				OljPrimedl = thetas[l][jprime][dprime[t1Config][l]];
+
+				System.out.println("Oljdl  " +l+" " +jprime+" "+ dprime[t1Config][l]+" "+OljPrimedl);
+
 				
 			}
 			
